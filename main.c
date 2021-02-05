370901afa9e3e73337ff90b6fb8c980d87d3f7ca
Terence646 <77753599+Terence646@users.noreply.github.com>
2021-02-05 12:50:46 -0700

    Add files via upload
    2nd Commit. Changed LED's, programed the MSP to blink once.

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
     P1->DIR |= BIT0;
-    P1->OUT &= ~BIT1;
+    P1->OUT &= ~BIT0;
+    P1->OUT |= BIT0;
 
 }
