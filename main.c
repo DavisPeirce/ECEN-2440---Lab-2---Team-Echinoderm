f6f559f49bcbf4366c15722313f4e9fedc5f1fe7
Terence646 <77753599+Terence646@users.noreply.github.com>
2021-02-08 16:24:21 -0700

    Add files via upload
    Working Version

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
-#include "msp.h"
-
+#include <msp430.h> 
+#include <stdint.h>
 
 /**
  * main.c
  */
-void main(void)
-{
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
-    P1->DIR |= BIT0;
-    P1->OUT &= ~BIT0;
-    P1->OUT |= BIT0;
 
+int main(void){
+
+    int a = 0;                                  //Initializing Variables
+    int b = 0;
+    uint16_t * memLoc;
+
+	WDTCTL = WDTPW | WDTHOLD;	            // stop watchdog timer
+                                           // Define a pointer that will iterate through the memory addresses
+    memLoc = 0x2001000A;                   //Starting location
+
+    for(memLoc; memLoc < 0x200103CC; memLoc++){   //For loop to look through each element in the desired memory range
+
+        if(*memLoc == 0x0000ACCA){            //Scan for the desired half-word pattern
+            a++;                            //Increment a
+        }
+
+        if(*memLoc == 0xABBAAFFA){          //Scan for the desired full-word pattern
+            b++;                            //Increment b
+        }
+    }
+	return 0;
 }
